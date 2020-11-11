b64ddd6ef468b98d1f7a9c73ab2f0d45d834af1c
Bartosz Tyrala <automationdream@gmail.com>
2020-11-11 18:00:06 +0100

    feat: home-route
    start from the scratch

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def dashboard():
+    return {"Dashboard": "Home Page"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
