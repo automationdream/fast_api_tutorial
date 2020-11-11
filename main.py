2f7e243b8e15c1ab1bbdbe1db3f5e16d5094757e
Bartosz Tyrala <automationdream@gmail.com>
2020-11-11 21:30:33 +0100

    feat: automatically create db schema using SQLAlchemy

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+import models
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 templates = Jinja2Templates(directory="templates")
 
